3233362a6b39bf734b8950092c0ab93322e47fc2
Raazi Muhammed <70052135+raazi-muhammed@users.noreply.github.com>
2024-01-04 19:41:44 +0530

    chore: set up Tailwind and shadcn ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
     return (
         <div>
+            <p className="bg-slate-100 p-4">Hlooo</p>
             <h1>Vite + React + Docker</h1>
+            <Button>Click me</Button>
         </div>
     );
 }
